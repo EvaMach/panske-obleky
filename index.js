7c3ef96544b31e6f8c9c2ddb223de55039e79751
Eva Machová <eva.machova@newtontech.cz>
2022-04-19 19:45:21 +0200

    Přidat barvy

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('funguju');
+const shirt = document.querySelector('svg')
+
+document.querySelector('.colors').innerHTML += `
+<div class="color">
+            <div class="color-box"></div>
+            <p class="color__name">černá</p>
+          </div>
+          <div class="color">
+            <div class="color-box"></div>
+            <p class="color__name">červená</p>
+          </div>
+          <div class="color">
+            <div class="color-box"></div>
+            <p class="color__name">nebesky modrá</p>
+          </div>`
